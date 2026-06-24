560f7e0e5098a2b881edbe172bcd2b0a6d383b49
Aisha AbuJoub <aisha.abujeib@gmail.com>
2026-06-25 00:07:35 +0300

    feat(auth): implement signup, login, JWT authentication, and role-based authorization

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -2,12 +2,35 @@ const express = require("express");
 
 const authController = require("./auth.controller");
 const authValidation = require("./auth.validation");
-const { authenticate } = require("../../middlewares/auth.middleware");
+const { authenticate, authorize } = require("../../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.post("/signup", authValidation.validateSignup, authController.signup);
 router.post("/login", authValidation.validateLogin, authController.login);
 router.get("/me", authenticate, authController.getMe);
+router.get(
+  "/admin-test",
+  authenticate,
+  authorize("ADMIN"),
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: "Welcome Admin",
+    });
+  }
+);
+
+router.get(
+  "/client-test",
+  authenticate,
+  authorize("CLIENT"),
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: "Welcome Client",
+    });
+  }
+);
 
 module.exports = router;
