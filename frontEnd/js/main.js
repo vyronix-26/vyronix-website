0ce4224b3491dd57d7790fa50c15fa4c962602c0
rubaabusamraalnajahcomputer12345 <rubaabusam@gmail.com>
2026-06-28 00:22:06 +0300

    First Design/Incomplete

--- frontEnd/js/main.js
+++ frontEnd/js/main.js
@@ -12,3 +12,32 @@ const chatBtn = document.querySelector(".chat-btn");
 chatBtn.addEventListener("click", () => {
     alert("Hello! I am VYRONIX smart assistant.");
 });
+// search functionality
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".digital-card");
+
+function filterCards(value) {
+    cards.forEach(card => {
+        const title = card.querySelector("h3").textContent.toLowerCase();
+
+        const match = title.includes(value);
+
+        card.style.display = match ? "flex" : "none";
+    });
+}
+
+// debounce عشان ما يشتغل على كل حرف بشكل مزعج
+function debounce(func, delay) {
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), delay);
+    };
+}
+
+const handleSearch = debounce(function (e) {
+    const value = e.target.value.toLowerCase().trim();
+    filterCards(value);
+}, 200);
+
+searchInput.addEventListener("input", handleSearch);
